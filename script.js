9e89fab83d5f67d758635ce8bb13f609e18ae35a
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2023-12-05 00:09:22 -0500

    add correct logic to the game

--- script.js
+++ script.js
@@ -17,18 +17,20 @@ function compare(player1, player2){
     switch (gameChoices.indexOf(player1)){
         case 0:
             if (player2 == gameChoices[1]){
-                return losePhrase(player2,player1);
+                return 1;
             }
+            return 2;
         case 1:
             if (player2 == gameChoices[2]){
-                return losePhrase(player2,player1);
+                return 1;
             }
+            return 2;
         case 2:
             if (player2 == gameChoices[0]){
-                return losePhrase(player2,player1);
+                return 1;
             }
         default:
-             return "You win! " + player1 + " beats " + player2;
+             return 2;
     }
 
 }
@@ -38,7 +40,7 @@ function playRound(playerSelection, computerSelection){
     let result = compare(playerSelection.toLowerCase(), computerSelection)
 
     if (result == 'tie') {
-        return ""
+        return 0;
     } else
     {
         return result;
@@ -46,14 +48,34 @@ function playRound(playerSelection, computerSelection){
 
 };
 
-console.log(getComputerChoice());
-console.log(compare("rock","paper"));
+function game(){
 
-// function game(){
+    console.log("Let's play best of 5")
+    let n=0
+    let wins = 0
+    while (n < 5) {
+        let player = prompt("What do you play?")
+        let computerSelection = getComputerChoice();
+        let result = playRound(player, computerSelection)
+        if (result === 0)
+        {
+            console.log("A tie! redo the round!")
+            continue;
+        } else if (result === 1) {
+            console.log(losePhrase(computerSelection,player));
+        } else
+        {
+            console.log("You win! " + player + " beats " +computerSelection);
+            wins++;
+        }
+        n++;
+    }
+    if (wins >= 3){
+        console.log("Congratulations you win the game!")
+    } else
+    {
+        console.log("You lost the game.")
+    }
+}
 
-//     let n=0
-//     while (n < 5) {
-//         prompt()
-//         n++;
-//     }
-// }
+game();
